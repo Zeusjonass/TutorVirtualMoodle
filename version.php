f9b113041583fca004f27723737bd054eeb52612
Zeus Sarmiento <zeus@yellowme.mx>
2020-04-23 17:58:32 -0500

    carpetas del esqueleto

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2020041700;         // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2020041600;         // Requires this Moodle version
+$plugin->requires  = 2019111200;         // Requires this Moodle version
 $plugin->component = 'tutorvirtual'; // Full name of the plugin (used for diagnostics)
