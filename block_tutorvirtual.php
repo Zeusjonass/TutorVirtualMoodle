6da9a09262eedb05bc9c8829e3bca4d919ea8577
MoisesVinajera <moisesvinajera@gmail.com>
2020-04-26 21:29:45 -0500

    Actualizado

--- block_tutorvirtual.php
+++ block_tutorvirtual.php
@@ -4,19 +4,19 @@ class block_tutorvirtual extends block_base {
     public function init() {
         $this->title = get_string('Títulooooooooooo', 'block_simplehtml');
     }
-    // The PHP tag and the curly bracket for the class definition 
+    // The PHP tag and the curly bracket for the class definition
     // will only be closed after there is another function added in the next section.
-    
+
     public function get_content() {
       if ($this->content !== null) {
         return $this->content;
       }
-   
+
       $course = $this->page->cm;
       $this->content         =  new stdClass;
-      $this->content->image  = 'Ya funcionaaaaanaaaaa <br><br> aaaaaaaa <br><br> xxxxxxxxx';
+      $this->content->text  = 'Ya funcionaaaaanaaaaa <br><br> aaaaaaaa <br><br> xxxxxxxxx';
       $this->content->footer = 'Holaaaaaaaa';
-   
+
       return $this->content;
   }
 }
