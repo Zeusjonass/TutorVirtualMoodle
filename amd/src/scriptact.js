55721b4aa7a045f8bad349e4dce73859125dd490
Andres-X <57787305+Andres-X@users.noreply.github.com>
2021-02-24 12:59:07 -0500

    Script para el menu

--- amd/src/scriptact.js
+++ amd/src/scriptact.js
@@ -1,44 +1,11 @@
-//alert("alerta de prueba");
-window.onclick = function(event) {
-  var imagen = document.getElementById('imagen');
-  var menu = document.getElementById('menu');
-  var menuActividades = document.getElementById('menuActividades');
-  var imprimirActividades = document.getElementById('imprimirActividades');
-  var enviarMensaje = document.getElementById('menuEnviarMensaje');
-  var inputMensaje = document.getElementById('inputMensaje');
-  var textfield = document.getElementById('textfield');
-  var menuRecursos = document.getElementById('menuRecursos');
-  var imprimirRecursos = document.getElementById('imprimirRecursos');
-  if(event.target == menuRecursos){
-    menu.style.display='none';
-    imprimirRecursos.style.display='block';
+function toggleMenu() {
+  var menuBox = document.getElementById('menu');    
+  if(menuBox.style.display == "block") {
+    menuBox.style.display = "none";
+    
   }
-  else{
-    imprimirRecursos.style.display='none';
-    if(event.target == enviarMensaje || event.target == textfield){
-      menu.style.display='none';
-      inputMensaje.style.display='block';
-    }
-    else{
-      inputMensaje.style.display='none';
-      if(event.target == menuActividades){
-        imprimirActividades.style.display='block';
-        menu.style.display='none';
-      }
-      else{
-        if(event.target != inputMensaje){
-          imprimirActividades.style.display='none';
-        }
-        if (event.target == imagen) {
-          if (menu.style.display=='block') {
-            menu.style.display='none';
-          }else {
-            menu.style.display='block';
-          }
-        }else {
-          menu.style.display='none';
-        }
-      }
-    }
+  else {
+    menuBox.style.display = "block";
   }
-};
+}
+
