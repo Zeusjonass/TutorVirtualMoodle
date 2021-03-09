e2d0c970c9b6c3bf8f24a2df9bf390d25c9c3761
Zeus Sarmiento <35499170+Zeusjonass@users.noreply.github.com>
2021-03-09 12:56:40 -0500

    Lang incluidos

--- lang/es/block_tutorvirtual.php
+++ lang/es/block_tutorvirtual.php
@@ -1,14 +1,43 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for the quizaccess_facialrecognition plugin.
+ *
+ * @package    block_tutorvirtual
+ * @copyright  2020 _____________________
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $string['pluginname'] = 'Tutor virtual';
 $string['tutorVirtual'] = 'Tutor Virtual';
 $string['title'] = '';
 $string['tutorVirtual:addinstance'] = 'Agrega un nuevo bloque de tutor virtual';
 $string['tutorVirtual:myaddinstance'] = 'Agrega un nuevo tutor virtual a tu pagina de Moodle';
 $string['faqCourse'] = 'Preguntas Frecuentes del Curso';
-$string['facMoodle'] = 'Preguntas Frecuentes de la Plataforma';
+$string['faqMoodle'] = 'Preguntas Frecuentes de la Plataforma';
 $string['savedQuestion'] = 'Muchas gracias por registrar su pregunta!';
 $string['activities'] = 'Actividades';
 $string['resources'] = 'Recursos';
+$string['books'] = 'Libros';
+$string['urls'] = 'URLs';
+$string['folders'] = 'Carpetas';
+$string['pages'] = 'Páginas';
+$string['labes'] = 'Etiquetas';
+$string['imscp'] = 'IMSCP';
 $string['msgTeacher'] = 'Mensaje al Profesor';
 $string['msgCaption'] = '¿Tienes alguna duda?<br>¡Envíale un mensaje a tu Profesor(a)!';
 $string['formDesc1'] = 'Hola! Soy el Tutor Virtual';
@@ -17,3 +46,71 @@ $string['formDesc3'] = 'Ayúdame a lograrlo ingresando en los siguientes campos 
 $string['questionSubject'] = 'Unidad de la pregunta:';
 $string['save'] = 'Guardar';
 $string['resources'] = 'Recursos';
+//Temas de las preguntas frecuentes de la plataforma
+$string['faqMoodleT1'] = 'Acceso y Navegación';
+$string['faqMoodleT2'] = 'Contenido de Curso';
+$string['faqMoodleT3'] = 'Tareas y Calificaciones';
+$string['faqMoodleT4'] = 'Exámenes';
+$string['faqMoodleT5'] = 'Correos y Foros';
+//Preguntas frecuentes de la plataforma
+$string['faqMoodleQ1'] = '¿Por qué no puedo acceder?';
+$string['faqMoodleQ2'] = '¿Cómo gano acceso a un curso?';
+$string['faqMoodleQ3'] = '¿Cómo salto entre mis cursos?';
+$string['faqMoodleQ4'] = '¿Cómo regreso a la página principal del curso?';
+$string['faqMoodleQ5'] = '¿Cómo puedo encontrar el curso X?';
+$string['faqMoodleQ6'] = '¿A dónde se han ido todos los temas/semanas?';
+$string['faqMoodleQ7'] = '¿Por qué no hay botón de "subir" (o "grabar")?';
+$string['faqMoodleQ8'] = '¿Cómo puedo ver los comentarios del profesor a mis tareas recientes?';
+$string['faqMoodleQ9'] = '¿Por qué mi promedio del curso es tan baja?';
+$string['faqMoodleQ10'] = '¿Qué botón pulso cuando he terminado el examen?';
+$string['faqMoodleQ11'] = '¿Porqué estoy obteniendo cero de calificación en mi examen?';
+$string['faqMoodleQ12'] = '¿Por qué no tengo ningún correo y otros usuarios sí?';
+$string['faqMoodleQ13'] = '¿Cómo puedo dejar de recibir todos estos correos?';
+//Respuestas a las preguntas frecuentes de la plataforma
+$string['faqMoodleA1'] = 'Podría haber muchas razones, pero la más probable es que simplemente haya olvidado su contraseña, 
+esté intentando acceder con una contraseña equivocada o la esté escribiendo incorrectamente. Algunas otras posibilidades son:<br><br>
+¿Contienen su nombre de usuario o contraseña una mezcla de MAYÚSCULAS y minúsculas?. Si es así, deberán ser escritas en la forma exacta.<br>
+¿Están habilitadas las cookies en su navegador?';
+$string['faqMoodleA2'] = 'Localice o busque el curso deseado (Usted puede elegir "Todos los cursos..." en el bloque de "Mis cursos") 
+y elija el nombre del curso. Si su profesor le ha dado una clave o contraseña para inscripción, escríbala en donde 
+corresponda y elija "Inscríbirme en este curso". Una vez que Usted esté insrito en el curso, aparecerá debajo de 
+"Mis cursos" cada vez que Usted ingrese al sitio Moodle.';
+$string['faqMoodleA3'] = 'Mediante su bloque de Mis Cursos si está disponible
+Vuelva a la página principal homepage (vea más adelante) y utilice el bloque de Mis Cursos del curso principal 
+(si está disponible!)';
+$string['faqMoodleA4'] = 'Utilice la Barra de navegación que está en la parte superior izquierda de la página, o use el botón en la parte 
+completamente inferior de la página del curso.';
+$string['faqMoodleA5'] = 'Si no está ya inscrito en un curso, puede buscarlo por el nombre y descripción.Si ya está inscrito le aparecerá 
+en su blog ( si está disponible).';
+$string['faqMoodleA6'] = 'Probablemente pulsó en el icono One.gif. Para descubrir todos los otros temas/semanas necesita pulsar el icono 
+All.gif que verá en el margen derecho de temas/semanas. Si los temas están colapsados puede utilizar el combo 
+desplegable y moverse por los temas/semanas que se muestran para saltar a una sección oculta.';
+$string['faqMoodleA7'] = 'Puede ser que:<br><br>
+
+La tarea esté cerrada en este momento.<br>
+La tarea no haya sido abierto aún.<br>
+Ya ha subido/grabado algo antes y la configuración impida que repita las tareas.';
+$string['faqMoodleA8'] = 'Hay muchas maneras de acceder a esos comentarios. El método más común consiste en ir al mismo sitio en 
+el que envió/subió el trabajo. Otro método consiste en pulsar en el vínculo que se encuentra en el bloque 
+"Actividades recientes", en el caso de que el profesor haya incluido ese bloque en su curso. Un método más 
+consistiría en accedere al libro de calificacionesy pinchar en el vínculo correspondiente a la tarea. 
+Dependiendo de cómo se haya configurado la tarea, puede recibir un correo electrónico si ha sido seleccionado 
+con un vínculo directo a los comentarios.';
+$string['faqMoodleA9'] = 'No se asuste. El sistema de evaluación de Moodle tiene en cuenta los trabajos no calificados y los pendientes 
+de envío. Dicho de otro modo: Usted empieza con un cero y a medida que vaya avanzando a través del curso y 
+completando las actividades evaluables su nota subirá poco a poco.';
+$string['faqMoodleA10'] = 'Depende de lo que quiera hacer...';
+$string['faqMoodleA11'] = 'Puede ser que haya excedido el tiempo límite para completar el examen. Esto nunca debería de suceder, porque 
+el cronómetro descendiente debería de enviar el examen automáticamente en cuanto se agote el tiempo, y después 
+el servidor Moodle debería de procesar su envío rápidamente. Sin embargo, si el servidor estuviera sobrecargado, 
+y corriendo lentamente, sus respuestas podrían no ser procesadas hasta después de un tiempo que sobrepasa el 
+margen permitido para envío del examen y a Usted no le estarán dando puntos para sus respuestas.';
+$string['faqMoodleA12'] = 'Es posible que su dirección de correo en su perfil esté errónea o deshabilitada. También pudiera ser que 
+no se haya suscrito a los foros que generan correos. Los usuarios AOL pueden no recibir correo tampoco 
+si el administrador ha bloqueado el uso de direcciones de correo AOL.';
+$string['faqMoodleA13'] = 'El correo electrónico es esencial para el funcionamiento de Moodle. Se emplea para mantenerte informado de 
+las novedades. Si quieres reducir la cantidad de correos que recibes puedes:<br><br>
+
+  Editar su perfil y cambiar su configuración de e-mail para recibir resúmenes.<br>
+  Cancelar su subscripción a foros no esenciales (¡aunque existen por algo!).<br>
+  Inhabilitar su dirección de correo electrónico de su perfil, aunque esto no recomendable y puede ir en contra de las reglas de la casa.';
